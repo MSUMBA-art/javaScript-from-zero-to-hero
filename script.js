560999be78b52adf3293ebf42dcea92c5e497b42
ALEXANDER <msumba83@gmail.com>
2021-12-12 17:50:16 +0200

    variable and variable naming

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let js = "amazing";
 console.log(40 + 40 + 80 - 100);
 
+console.log("Alex");
+console.log(38);
 
+let firstName = "Alex";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
