9119b1c2032e4517437a7f7d41eb8cd2136c3872
ALEXANDER <msumba83@gmail.com>
2021-12-13 12:43:09 +0200

    Data types

---
+++ assignments.js
@@ -0,0 +1,7 @@
+// let country = "Malawi";
+// let continent = "Africa";
+// let population = 13000000
+
+// console.log("My country is " + country);
+// console.log(continent);
+// console.log("The population is " + population);
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let js = "amazing";
+/*let js = "amazing";
 console.log(40 + 40 + 80 - 100);
 
 console.log("Alex");
@@ -8,3 +8,15 @@ let firstName = "Alex";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 38);
+console.log(typeof "Alex");
+
+javascriptIsFun = "YES";
+console.log(typeof javascriptIsFun);
