1c91f36beab80ad509dc8da010c353eb9e14c52f
ALEXANDER <msumba83@gmail.com>
2021-12-14 14:47:54 +0200

    math operators

--- assignments.js
+++ assignments.js
@@ -1,7 +1,16 @@
-// let country = "Malawi";
+// VALUES AND VARIABLES
+let country = "Malawi";
 // let continent = "Africa";
-// let population = 13000000
+let population = 13000000
 
 // console.log("My country is " + country);
 // console.log(continent);
 // console.log("The population is " + population);
+
+// DATA TYPES
+// let isIsland = true;
+// let language 
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
--- script.js
+++ script.js
@@ -10,13 +10,53 @@ console.log(firstName);
 console.log(firstName);
 */
 
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
 
-console.log(typeof true);
-console.log(typeof javascriptIsFun);
-console.log(typeof 38);
-console.log(typeof "Alex");
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 38);
+// console.log(typeof "Alex");
 
-javascriptIsFun = "YES";
-console.log(typeof javascriptIsFun);
+// javascriptIsFun = "YES";
+// console.log(typeof javascriptIsFun);
+
+// let age = 38;
+// age = 40; // reasign s valuable
+
+// let birthday = 1983;
+// birthday = 1990;
+
+// var job = "programmer";
+// job = "driver";
+
+// BASIC OPERATORS
+// A math operators
+const now = 2025;
+const ageAlex = now - 1983;
+const ageIrene = now - 2012;
+console.log(ageAlex, ageIrene);
+
+console.log(ageAlex * 10 / 100, ageAlex / 2, 2 ** 3);
+
+const firstName = "Alex";
+const lastName = "Msumba";
+console.log(firstName + " " + lastName);
+
+// B assignment operators
+let x = 10 + 5; // 15
+x += 10 // x = x 10 = 25
+x *= 4 //x = x * 4 = 100
+x++ // x = x + 1 = 101   increase the value by 1
+x--; // decrease the value by 1
+x--;
+console.log(x);
+
+// B comparison operators
+
+console.log(ageAlex > ageIrene); // >, <, >=, <=
+console.log(ageIrene >= 10);
+
+const isFullAge = ageIrene >= 10;
+
+console.log(now - 1999 > now - 2020);
