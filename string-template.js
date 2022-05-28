f3b0eb64958936cc7624a06ade24693c59ba21b9
ALEXANDER <msumba83@gmail.com>
2022-05-28 09:09:06 +0200

    strings and template literals

---
+++ string-template.js
@@ -0,0 +1,17 @@
+const firstName = "Alexander";
+const job = "driver";
+const birthYear = 1983;
+const year = 2022;
+
+// const alexander = "I am " + firstName + ", a " + (year - birthYear) + " years old" + " " + job;
+// console.log(alexander);
+
+const alexNew = `I am ${firstName} , a ${year - birthYear} year old ${job}` ;
+console.log(alexNew);
+//console.log(`jjjjjjj jjjj`); we can use back ticks for a regular string
+
+console.log("string with \n\ multiple \n\ lines");
+
+console.log(`string
+multiple
+lines`);
