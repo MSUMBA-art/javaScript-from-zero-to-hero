aab8e6eca7473e92f6948ceafba0f4140f20b740
ALEXANDER <msumba83@gmail.com>
2022-07-27 06:54:12 +0200

    initial commit

--- equalityOps.js
+++ equalityOps.js
@@ -1,25 +1,39 @@
 const age = 18;
-if (age == 18) {
+if (age === 18) {
   console.log("adult");  
 }
 
-console.log('18' == 18)// true;
-console.log('18' === 18)// false;
+ console.log('18' == 18)// true;
+ console.log('18' === 18)// false;
 
-const favourite = Number("What is your favorite number")
-console.log(favourite);
-console.log(typeof favourite);
+const favourite = 99;
+ console.log(favourite);
+ console.log(typeof favourite);
+
 
 if (favourite === 23) {
-  console.log("23 is cool");
-} else if(favourite === 7) {
-  console.log("7 is also cool");
+  console.log("23 is amazing");
+} else if (favourite === 7) {
+  console.log("7 is cool");
+
 } else if (favourite === 9) {
-  console.log("9 is cool too");
-} else {
-  console.log("all are not numbers");
+  console.log("9 is cool zedi");
 }
 
-if ( favourite !== 23) { // !== means not
+else { 
+  console.log("Number is not 23 or 7 or 9");
+ }
+
+//  if (favourite === 23) {
+//   console.log("23 is cool");
+//  } else if(favourite === 7) {
+//   console.log("7 is also cool");
+//  } else if (favourite === 9) {
+//    console.log("9 is cool too");
+// } else {
+//    console.log("all are not numbers");
+//  }
+
+//  if ( favourite !== 23) { // !== means not
   
-}
+//  }
