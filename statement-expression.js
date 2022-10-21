4a0c7d6779276da84335e7855b601749ca65b39c
ALEXANDER <msumba83@gmail.com>
2022-10-21 07:00:08 +0200

    done statement and expression

---
+++ statement-expression.js
@@ -0,0 +1,8 @@
+const alexAge = 100 - 50;
+const berthaAge = 200 - 150;
+console.log(`I am ${alexAge} years old and bertha is ${berthaAge} years old`);
+if (alexAge > berthaAge) {
+    console.log("MY WIFE");
+} else {
+    console.log("ENEMIES");
+}
