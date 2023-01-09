9d8e9bb6647a8568fa4c26a0f97b7a6962c1c5f3
ALEXANDER <msumba83@gmail.com>
2023-01-09 09:19:42 +0200

    initial commit

---
+++ objects.js
@@ -0,0 +1,18 @@
+const car = [
+    "Alex",
+    "Msumba",
+    2023 - 1983,
+    "Self Employed",
+    ["Duncan", "Mary"],
+];
+
+const alex = {
+    firstName: "Alexander Junior",
+    lastName: "Msumba",
+    age: 2023 - 1983,
+    job: "Self employed",
+    friends: ["Duncan", "Mary"]
+}
+
+console.log(alex);
+console.log(alex);
