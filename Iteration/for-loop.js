c0ad5f997f2d22623e91f56c53dd2fdcd6a1c305
ALEXANDER <msumba83@gmail.com>
2023-01-19 14:56:22 +0200

    introduction to for-loop

---
+++ Iteration/for-loop.js
@@ -0,0 +1,9 @@
+// console.log("listening weight 1");
+// console.log("listening weight 2");
+// console.log("listening weight 3");
+// console.log("listening weight 4");
+
+// for loop keeps on running while the condition is TRUE
+for (let rep = 1; rep <= 11; rep++) {
+    console.log(`listening weight ${rep}`);
+}
